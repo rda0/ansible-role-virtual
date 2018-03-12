9d100d64e1da047b2d9a5dbfa9ecfe472e2a215a
Sven Mäder <maeder@phys.ethz.ch>
2018-03-12 18:58:07 +0100

    Add lvm install

---
+++ templates/install-vm-lvm.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ ! -f /etc/libvirt/qemu/{{ guest_name }}.xml ]; then
+
+virt-install \
+--virt-type=kvm \
+--cpu=host \
+--name={{ guest_name }} \
+--vcpus={{ vcpus }} \
+--memory={{ memory }} \
+--memorybacking=hugepages=yes \
+--memballoon=virtio \
+--controller=type=scsi,model=virtio-scsi \
+--disk=path=/dev/r10/{{ guest_name }}-root,bus=scsi,cache=none \
+--location={{ location }} \
+--os-type=linux \
+--os-variant={{ os_variant }} \
+--console=pty,target_type=serial \
+--initrd-inject={{ vm_path }}/{{ guest_name }}/preseed.cfg \
+--extra-args='auto=true priority=critical console=ttyS0,115200n8 serial elevator=noop' \
+--boot=kernel_args="elevator=noop" \
+{% if mac is defined %}
+--network=bridge={{ bridge }},model=virtio,mac={{ mac }} \
+{% else %}
+--network=bridge={{ bridge }},model=virtio \
+{% endif %}
+--nographics \
+--noautoconsole \
+--autostart
+
+#--hvm \ # full virtualized
+#--paravirt \ # paravirtualized
+
+else
+    echo "vm already defined!"
+    exit 1
+fi
