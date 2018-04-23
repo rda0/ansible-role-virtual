eb978984ed46a5f7d6378ea25b87eaa59401a858
Sven Mäder <maeder@phys.ethz.ch>
2018-04-23 14:40:08 +0200

    Modify hostname to osiris

--- templates/ansible-notify.sh
+++ templates/ansible-notify.sh
@@ -2,4 +2,4 @@
 
 HOSTNAME="$(hostname -s)"
 IP="$(host ${HOSTNAME} | grep -iEo '[.0-9]+$')"
-GET "http://phd-kvm1.ethz.ch/webapp/index?hostname=${HOSTNAME}&ip=${IP}"
+GET "http://osiris.ethz.ch/webapp/index?hostname=${HOSTNAME}&ip=${IP}"
