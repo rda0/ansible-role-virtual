c833178b40f7476d7e947cf4f9a03c0b24d35b78
Sven Mäder <maeder@phys.ethz.ch>
2019-03-27 11:14:39 +0100

    Add cpu and memory hotplugging to all tasks

--- templates/install-vm-lvm.sh
+++ templates/install-vm-lvm.sh
@@ -3,13 +3,28 @@
 if [ ! -f /etc/libvirt/qemu/{{ virtual_guest_name }}.xml ]; then
 
 virt-install \
---virt-type=kvm \
---cpu=host \
 --name={{ virtual_guest_name }} \
---vcpus={{ virtual_cpus }} \
+--virt-type=kvm \
+{% if virtual_memory_hotpluggable %}
+--cpu=model=host,cell0.id=0,cell0.cpus={{ virtual_numa_cell_cpus }},cell0.memory={{ virtual_numa_cell_memory }} \
+--memory=memory={{ virtual_memory }},hotplugmemorymax={{ virtual_memory_max }},hotplugmemoryslots={{ virtual_memory_slots }} \
+{% else %}
+--cpu=host \
 --memory={{ virtual_memory }} \
---memorybacking=hugepages=yes \
+{% endif %}
+{% if virtual_cpus_hotpluggable %}
+--vcpus=vcpus={{ virtual_cpus }},maxvcpus={{ virtual_cpus_max }} \
+{% else %}
+--vcpus={{ virtual_cpus }} \
+{% endif %}
+{% if virtual_memory_hugepages %}
+--memorybacking=hugepages=yes,size={{ virtual_memory_hugepages_size }},unit={{ virtual_memory_hugepages_unit }} \
+{% endif %}
+{% if virtual_memory_balloon %}
 --memballoon=virtio \
+{% else %}
+--memballoon=none \
+{% endif %}
 --controller=type=scsi,model=virtio-scsi \
 --disk=path=/dev/{{ virtual_disk_vg }}/{{ virtual_guest_name }}-root,bus=scsi,cache=none \
 --location={{ location }} \
@@ -20,9 +35,9 @@ virt-install \
 --extra-args='auto=true priority=critical elevator=noop net.ifnames=0 biosdevname=0 nousb console=tty0 console=ttyS0,115200' \
 --boot=kernel_args="elevator=noop net.ifnames=0 biosdevname=0 nousb console=tty0 console=ttyS0,115200" \
 {% if virtual_mac is defined %}
---network=bridge={{ virtual_bridge }},model=virtio,mac={{ virtual_mac }} \
+--network=bridge={{ virtual_bridge }},model=virtio,target={{ virtual_interface_name }},mac={{ virtual_mac }} \
 {% else %}
---network=bridge={{ virtual_bridge }},model=virtio \
+--network=bridge={{ virtual_bridge }},model=virtio,target={{ virtual_interface_name }} \
 {% endif %}
 --nographics \
 --noautoconsole \
